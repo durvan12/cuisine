8c6d3c98e0c2eb0b6c8e3f4c81d7c53205ee6eb9
durvan12 <abbassi.hassen23@gmail.com>
2022-01-04 21:57:52 +0100

    rajouter salé et sucré , corriger user et Category

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -221,4 +221,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+    public function __toString()
+    {
+        return $this->email;
+    }
+
 }
