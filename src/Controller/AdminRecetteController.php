6a7485d5431378f42fa30669b2298562f5ba40ff
durvan12 <abbassi.hassen23@gmail.com>
2022-01-06 11:33:11 +0100

    controller recette form et template category

--- src/Controller/AdminRecetteController.php
+++ src/Controller/AdminRecetteController.php
@@ -2,16 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Entity\Recette;
 use App\Form\RecetteType;
 use App\Repository\RecetteRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Security;
-use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin/recette')]
 class AdminRecetteController extends AbstractController
@@ -25,7 +26,7 @@ class AdminRecetteController extends AbstractController
     }
 
     #[Route('/new', name: 'admin_recette_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager, Security $security): Response
+    public function new(Request $request, ManagerRegistry $managerRegistry): Response
     {
         $recette = new Recette();
         $form = $this->createForm(RecetteType::class, $recette);
@@ -33,20 +34,21 @@ class AdminRecetteController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-            $user = $security->getUser();
-            $recette->setAuthor($user);
+            $infoImg = $form['img']->getData(); // récupère les informations de l'Img 1
+            $extensionImg = $infoImg->guessExtension(); // récupère l'extension de fichier de l'Img 1
+            $nomImg = time() . '-1.' . $extensionImg; // reconstitue un nom d'Img unique pour l'Img 1
+            $infoImg->move($this->getParameter('recettes_pictures_directory'), $nomImg); // déplace l'Img 1 dans le dossier adéquat
+            $recette->setImg($nomImg); // définit le nom de l'image 2 à mettre en base de données
 
-            // gestion de l'img (vérifs, renommer avec timestamp, upload, définition pour la bdd = setImg())
-            
-            $entityManager->persist($recette);
-            $entityManager->flush();
-
+            $manager = $managerRegistry->getManager(); // récupère le manager de Doctrine
+            $manager->persist($recette); // dit à Doctrine qu'on va vouloir sauvegarder en bdd
+            $manager->flush(); // exécute la requête
+            $this->addFlash('success', 'La recette a bien été ajoutée'); // génère un message flash
             return $this->redirectToRoute('admin_recette_index', [], Response::HTTP_SEE_OTHER);
         }
-
         return $this->renderForm('admin_recette/new.html.twig', [
             'recette' => $recette,
-            'form' => $form,
+            'form' => $form, // création de la vue du formulaire et envoi à la vue (fichier)
         ]);
     }
 
--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -5,16 +5,46 @@ namespace App\Form;
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Image;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('content')
-        ;
+        ->add('title', TextType::class, [
+            'label' => 'Titre',
+            'attr' => [
+                'placeholder' => 'Ex.: nom du plat'
+            ]
+        ])
+        ->add('content', TextType::class, [
+            'attr' => [
+                'placeholder' => 'Ex.: ingredient et recette'
+            ]
+        ])
+        ->add('img', FileType::class, [
+            'required' => false,
+            'label' => 'Image principale',
+            'mapped' => false,
+            'help' => 'png, jpg, jpeg ou jp2 - 1 Mo maximum',
+            'constraints' => [
+                new Image([
+                    'maxSize' => '1024k',
+                    'mimeTypes' => [
+                        'image/png',
+                        'image/jpg',
+                        'image/jpeg',
+                        'image/jp2',
+                    ],
+                    'mimeTypesMessage' => 'Merci de sélectionner une iamge au format PNG, JPG, JPEG ou JP2'
+                ])
+            ]
+        ])
+    ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
